633bdc0f5764db923c996e6d1c970afc584fc2d2
zufzuf <muhammadalizufan@gmail.com>
2023-12-01 21:53:13 +0700

    Learn Collection In Java

--- src/CollectionInterface/ArrayApp.java
+++ src/CollectionInterface/ArrayApp.java
@@ -3,6 +3,12 @@ package CollectionInterface;
 import java.util.Arrays;
 import java.util.List;
 
+// # Collection interface
+//   - The Collection interface has a toArray()
+//     method to convert a collection to an Array.
+//   - This is very suitable if we want to change a collection to an Array,
+//     for example, because we want to call a method whose parameters are of the type array, not collection.
+//
 public class ArrayApp {
   public static void main(String[] args) {
     List<String> names = List.of("August", "De", "Caprio");
